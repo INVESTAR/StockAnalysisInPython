e3dfd4144450f539fbcc18dbf40781ea023b1766
HwangHoo Kim <hwanghoo@gmail.com>
2020-03-29 16:31:36 +0900

    Update ch03_DowKospi_Scatter.py

--- 03 NumPy and Pandas/ch03_DowKospi_Scatter.py
+++ 03 NumPy and Pandas/ch03_DowKospi_Scatter.py
@@ -6,13 +6,13 @@ yf.pdr_override()
 dow = pdr.get_data_yahoo('^DJI', '2000-01-04')
 kospi = pdr.get_data_yahoo('^KS11', '2000-01-04')
 
-df = pd.DataFrame({'DOW' dow['Close'], 'KOSPI' kospi['Close']}) # ①
-df = df.fillna(method='bfill') # ②
+df = pd.DataFrame({'DOW' dow['Close'], 'KOSPI' kospi['Close']})
+df = df.fillna(method='bfill')
 df = df.fillna(method='ffill')
 
 import matplotlib.pyplot as plt
 plt.figure(figsize=(7, 7))
-plt.scatter(df['DOW'], df['KOSPI'], marker='.') # ③
+plt.scatter(df['DOW'], df['KOSPI'], marker='.')
 plt.xlabel('Dow Jones Industrial Average')
 plt.ylabel('KOSPI')
 plt.show()
