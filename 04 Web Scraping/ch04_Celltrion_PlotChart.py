17d0b11dfc68828949c0f73f68a2a9bf377bf944
HwangHoo Kim <hwanghoo@gmail.com>
2020-03-30 12:00:11 +0900

    Update ch04_Celltrion_PlotChart.py

--- 04 Web Scraping/ch04_Celltrion_PlotChart.py
+++ 04 Web Scraping/ch04_Celltrion_PlotChart.py
@@ -3,7 +3,7 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from matplotlib import pyplot as plt
 
-url = 'httpsfinance.naver.comitemsise_day.nhncode=068270&page=1'
+url = 'https://finance.naver.com/item/sise_day.nhn?code=068270&page=1'
 with urlopen(url) as doc
     html = BeautifulSoup(doc, 'lxml') 
     pgrr = html.find('td', class_='pgRR')
@@ -11,16 +11,16 @@ with urlopen(url) as doc
     last_page = s[-1]  
 
 df = pd.DataFrame()
-sise_url = 'httpsfinance.naver.comitemsise_day.nhncode=068270'  
+sise_url = 'https://finance.naver.com/item/sise_day.nhn?code=068270'  
 for page in range(1, int(last_page)+1)
     page_url = '{}&page={}'.format(sise_url, page)  
     df = df.append(pd.read_html(page_url, header=0)[0])
 
 df = df.dropna()
-df = df.iloc[030]  # ①
-df = df.sort_values(by='날짜')  # ②
+df = df.iloc[030] 
+df = df.sort_values(by='날짜') 
 
 plt.title('Celltrion (close)')
-plt.xticks(rotation=90)  # ③
-plt.plot(df['날짜'], df['종가'], 'co-')  # ④
+plt.xticks(rotation=90) 
+plt.plot(df['날짜'], df['종가'], 'co-') 
 plt.show()
