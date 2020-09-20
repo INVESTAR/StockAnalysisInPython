a53d132f6222c8b1d232536a7035771cbc3c0e85
darrenlee <dongyup1.lee@samsung.com>
2020-09-20 17:52:23 +0900

    sync 13inch

--- 03_NumPy_and_Pandas/ch03_02_DowKospi_Scatter.py
+++ 03_NumPy_and_Pandas/ch03_02_DowKospi_Scatter.py
@@ -6,6 +6,8 @@ yf.pdr_override()
 dow = pdr.get_data_yahoo('^DJI', '2000-01-04')
 kospi = pdr.get_data_yahoo('^KS11', '2000-01-04')
 
+dow
+
 df = pd.DataFrame({'DOW' dow['Close'], 'KOSPI' kospi['Close']})
 df = df.fillna(method='bfill')
 df = df.fillna(method='ffill')
