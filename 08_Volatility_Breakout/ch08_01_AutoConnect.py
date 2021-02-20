18c3961620c6e43d01afa0f0e6821422b034c9b8
HwangHoo Kim <hwanghoo@gmail.com>
2021-02-21 08:07:14 +0900

    Update ch08_01_AutoConnect.py

--- 08_Volatility_Breakout/ch08_01_AutoConnect.py
+++ 08_Volatility_Breakout/ch08_01_AutoConnect.py
@@ -4,10 +4,15 @@ import os
 
 os.system('taskkill /IM coStarter* /F /T')
 os.system('taskkill /IM CpStart* /F /T')
+os.system('taskkill /IM DibServer* /F /T')
 os.system('wmic process where "name like \'%coStarter%\'" call terminate')
 os.system('wmic process where "name like \'%CpStart%\'" call terminate')
+os.system('wmic process where "name like \'%DibServer%\'" call terminate')
 time.sleep(5)        
 
+os.system('taskkill /IM coStarter* /F /T')
+os.system('taskkill /IM CpStart* /F /T')
+
 app = application.Application()
-app.start('C:\CREON\STARTER\coStarter.exe /prj:cp /id:hwangho0 /pwd:nasca0.. /pwdcert:5099jina.. /autostart')
+app.start('C:\CREON\STARTER\coStarter.exe /prj:cp /id:userid /pwd:pa$$word /pwdcert:certPa$$word /autostart')
 time.sleep(60)
