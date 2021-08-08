2879af216fe9480963f87e221c5646334b0435cf
HwangHoo Kim <hwanghoo@gmail.com>
2021-08-08 15:29:13 +0900

    Update ch04_01_Celltrion_PlotChart.py
    updated to send browser information using requests

--- 04_Web_Scraping/ch04_01_Celltrion_PlotChart.py
+++ 04_Web_Scraping/ch04_01_Celltrion_PlotChart.py
@@ -1,27 +1,33 @@
+# ch04_01_Celltrion_PlotChart.py
 import pandas as pd
-from urllib.request import urlopen
+import requests
 from bs4 import BeautifulSoup
 from matplotlib import pyplot as plt
 
+# 4.4.3 맨 뒤 페이지 숫자 구하기
 url = 'https://finance.naver.com/item/sise_day.nhn?code=068270&page=1'
-with urlopen(url) as doc:
-    html = BeautifulSoup(doc, 'lxml') 
-    pgrr = html.find('td', class_='pgRR')
-    s = str(pgrr.a['href']).split('=')
-    last_page = s[-1]  
+html = requests.get(url, headers={'User-agent': 'Mozilla/5.0'}).text
+bs = BeautifulSoup(html, 'lxml')
+pgrr = bs.find('td', class_='pgRR')
+s = str(pgrr.a['href']).split('=')
+last_page = s[-1]  
 
+# 4.4.4 전체 페이지 읽어오기
 df = pd.DataFrame()
 sise_url = 'https://finance.naver.com/item/sise_day.nhn?code=068270'  
 for page in range(1, int(last_page)+1):
-    page_url = '{}&page={}'.format(sise_url, page)  
-    df = df.append(pd.read_html(page_url, header=0)[0])
+    url = '{}&page={}'.format(sise_url, page)  
+    html = requests.get(url, headers={'User-agent': 'Mozilla/5.0'}).text
+    df = df.append(pd.read_html(html, header=0)[0])
 
+# 차트 출력을 위해 데이터프레임 가공하기
 df = df.dropna()
-df = df.iloc[0:30] 
-df = df.sort_values(by='날짜') 
+df = df.iloc[0:30]  # ①
+df = df.sort_values(by='날짜')  # ②
 
+# 날짜, 종가 컬럼으로 차트 그리기
 plt.title('Celltrion (close)')
-plt.xticks(rotation=45) 
-plt.plot(df['날짜'], df['종가'], 'co-')
+plt.xticks(rotation=45)  # ③
+plt.plot(df['날짜'], df['종가'], 'co-')  # ④
 plt.grid(color='gray', linestyle='--')
 plt.show()
