6a80b1abb0cf857d71079c8bbc751d020e37a3d2
HwangHoo Kim <hwanghoo@gmail.com>
2023-01-23 23:15:26 +0900

    Update ch02_01_ImageProcessing.py

--- 02_Python_Programming/ch02_01_ImageProcessing.py
+++ 02_Python_Programming/ch02_01_ImageProcessing.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 # 2.8.1 리퀘스트로 인터넷에서 이미지 파일 가져오기
-url = 'http://bit.ly/2JnsHnT'
+# url = 'http://bit.ly/2JnsHnT'
+url = 'http://aron-tr.com/default/img/ARON_2023/imgs/ShirohigeFalls.png'
 r = requests.get(url, stream=True).raw
 
 # 2.8.2 필로우로 이미지 보여주기
